475997b5e7543cb7387f30aded1d94fe2da6e257
mecennas <jrafael.pfz@gmail.com>
2022-03-18 16:25:45 -0300

    [feature] - create src & components & header component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import Header from './components/header';
+import { styles } from './styles';
+
+const App = () => {
+ 
+
+  return (
+    <View style={styles.container}>
+      <Header title='Bienvenidos'/>
+    </View>
+  );
+};
+
+export default App;
